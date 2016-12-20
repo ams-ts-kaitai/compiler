53d668f521209c40958cbdda2efef31dc0572939
Thorsten Schöning <tschoening@users.noreply.github.com>
2016-12-20 15:55:43 +0100

    I thought that mapping a value instance to an enum doesn't work properly, because I got invalid Java code which always used Enum.byId, even if I was already providing two enum instances in the value instance. So I've added some code in InstanceSpec to recognize this case and dont' use Enum.byId.
    That was stupid, because the enum keyword is used to map numerical values to enums. If I know I'm already using enum instances, I simply need to not use that keyword and the type detection for the value instance already recognizes the correct enum types even in my "... ? ... : ...".
    
    So I'm reverting the former changes to get inline with the upstream codebase again.

--- shared/src/main/scala/io/kaitai/struct/format/InstanceSpec.scala
+++ shared/src/main/scala/io/kaitai/struct/format/InstanceSpec.scala
@@ -20,20 +20,12 @@ object InstanceSpec {
         // value instance
         // TODO: check conflicts with all other keys
 
-        // Wrap everything in EnumById if "enum" is used and "value" contains an expression using
-        // integers, but take care of exceptions like "EnumByLabel" in some situations as well.
+        // Wrap everything in EnumById if "enum" is used
         val value2 = ParseUtils.getOptValueStr(srcMap, "enum", path) match {
           case None =>
             value
           case Some(enumName) =>
-            value match {
-              case Ast.expr.IfExp(_, ifTrue: Ast.expr, _) =>
-                ifTrue match {
-                  case Ast.expr.EnumByLabel(_,_) => value
-                  case _ => Ast.expr.EnumById(Ast.identifier(enumName), value)
-                }
-              case _ => Ast.expr.EnumById(Ast.identifier(enumName), value)
-            }
+            Ast.expr.EnumById(Ast.identifier(enumName), value)
         }
 
         val ifExpr = ParseUtils.getOptValueStr(srcMap, "if", path).map(Expressions.parse)
