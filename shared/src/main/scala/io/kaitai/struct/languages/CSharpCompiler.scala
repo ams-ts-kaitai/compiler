78ac0b0ceb1f2714aca8500ce260f24347a92046
Thorsten Schöning <tschoening@am-soft.de>
2020-05-06 15:26:28 +0200

    scala.MatchError: CalcArrayType(UserTypeInstream(List(par_oms_apl_full_vdb),None,List())) (of class io.kaitai.struct.datatype.DataType$CalcArrayType)

--- shared/src/main/scala/io/kaitai/struct/languages/CSharpCompiler.scala
+++ shared/src/main/scala/io/kaitai/struct/languages/CSharpCompiler.scala
@@ -613,6 +613,7 @@ object CSharpCompiler extends LanguageCompilerStatic
       case EnumType(name, _) => types2class(name)
 
       case ArrayType(inType) => s"List<${kaitaiType2NativeType(inType)}>"
+      case CalcArrayType(inType) => s"List<${kaitaiType2NativeType(inType)}>"
 
       case st: SwitchType => kaitaiType2NativeType(st.combinedType)
     }
